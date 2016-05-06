3c13fa195a1f0f16d7d2ca9a5f93c32e1edc70d9
goodlanlei <122901668@qq.com>
2016-05-07 00:16:40 +0800

    classroom 添加视频播放  2016-5-6

---
+++ student/common/js/classroom.js
@@ -0,0 +1,12 @@
+$(function () {
+	//视频显示
+	$(".cont table span").click(function(){
+			$(".next_page").show();
+	});
+	$(".next_page").click(function(){
+			$(".next_page").hide();
+	});
+	
+});
+
+
