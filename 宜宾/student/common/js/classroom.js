b7be4cf084c5378be4d5af0ce74c939e230c55bb
goodanlei <122901668@qq.com>
2016-05-11 15:43:41 +0800

    宜宾  student  样式调整

--- 宜宾/student/common/js/classroom.js
+++ 宜宾/student/common/js/classroom.js
@@ -1,10 +1,13 @@
 $(function () {
 	//视频显示
-	$(".cont table span").click(function(){
+	$(".cont table span.look").live("click",function(){
 			$(".next_page").show();
 	});
-	$(".next_page .show .tit img").click(function(){
+	$(".next_page .show .tit img").live("click",function(){
 			$(".next_page").hide();
+	});
+	$(".cont table span.down").live("click",function(){
+		alert("下载");
 	});
 	
 });
