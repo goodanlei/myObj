7801912b57d7c6e70916c4311802783148b86503
goodanlei <122901668@qq.com>
2016-05-19 16:23:34 +0800

    宜宾  教师版   。。。

--- 宜宾/teacher/common/ckeditor/config.js
+++ 宜宾/teacher/common/ckeditor/config.js
@@ -1,7 +1,8 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	
-  
+  	//config.removeDialogTabs = 'image:advanced;image:Upload;image:Link';
+  	config.filebrowserUploadUrl="actions/ckeditorUpload"; 
     config.extraPlugins = 'eqneditor';
    	config.toolbarGroups = [
 	
