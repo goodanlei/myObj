8e8f576a382263bec537e36f2549b1f48db3a75b
goodanlei <122901668@qq.com>
2016-05-23 10:18:15 +0800

    宜宾  学生版   tree

--- 宜宾/student/common/js/knowledgebase.js
+++ 宜宾/student/common/js/knowledgebase.js
@@ -70,11 +70,54 @@ $(function(){
 	   		$(".know_box_2").show();
 	   	});
 	   },
+	   //tree
+	   tree:function(){
+	   	var tree = $("#tree dl");
+	   	var _tit = tree.children("dt");
+	   	var tree_list_tit=tree.find("dt");
+	   	
+	   	//new  list
+	   	var list = [
+	   	{id:1,name:'第一章',item_s:0},
+	   	{id:1,name:'第2章',item_s:1},
+	   	{id:1,name:'第3章',item_s:0},
+	   	{id:1,name:'第4章',item_s:1},
+	   	{id:1,name:'第5章',item_s:0},
+	   	];
+	   	
+	   	
+	   	tree_list_tit.live("click",function(){
+	   		//show hide
+	   		$(this).parent().siblings().children("dt").next().hide();
+	   		console.log($(this).parent().siblings().size());
+	   		$(this).next().show();
+	   		tree.find(".active").removeClass("active");
+	   		$(this).addClass("active");
+	   		
+	   		//add  new  list
+	   		var s = $(this).attr("s");
+	   		var add_s = $(this).attr("add_s");
+	   		if(s*1&&(add_s==0||add_s== undefined)){
+		   		var ht = addList(list);
+		   		$(this).siblings().append(ht);
+		   		$(this).attr("add_s",1);
+	   		}
+	   	});
+	   	function addList(arr){
+	   		var add_list_html = '';
+	   		$.each(arr,function(k,v){
+	   			add_list_html +="<dl><dt s="+v.item_s+" class='have_"+v.item_s+"'><span>"+v.name+"</span></dt><dd></dd></dl>";
+	   		});		
+	   		return add_list_html;
+	   	}
+	   	
+	   },
        init:function(){
            this.show();
            this.exclusive();
            this.closeAll();
            this.search();
+           this.tree();
        },
    };
     knowledge.init();
