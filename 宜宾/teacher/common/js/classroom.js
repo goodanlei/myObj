6cbcf858e4dafe913fb390726a5a9270c883d2f1
goodanlei <122901668@qq.com>
2016-05-30 13:17:46 +0800

    样式修改  t

--- 宜宾/teacher/common/js/classroom.js
+++ 宜宾/teacher/common/js/classroom.js
@@ -144,6 +144,7 @@ $(function(){
 		_next.remove();
 		_next_remove.remove();
 	});
+	
 	$(".List-questions img.up").live('click',function(){
 		var max_num = $(".List-questions img.next").size();
 		var _this = $(this).parents(".ques_num");
@@ -245,6 +246,35 @@ $(function(){
 			$(".del_down").hide(); 
 		},1000);
 	});
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 });
 
 
@@ -340,69 +370,4 @@ window.onload = function(){
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
 }
-/*----------------------------------------------------- Ztree相关js ------------------------------------------------------------*/
-var setting = {};
-var zNodes =[
-    //有子分类
-    { name:"第一单元", open:true,
-        children: [
-            { name:"阅读",open:true,
-                children: [
-                    { name:"金色的草地"},
-                    { name:"金色的草地"},
-                    { name:"金色的草地"},
-                    { name:"金色的草地"}
-                ]
-            },
-        ]
-    },
-    { name:"第二单元",
-        children: [
-            { name:"阅读",
-                children: [
-                    { name:"金色的草地"},
-                    { name:"金色的草地"},
-                    { name:"金色的草地"},
-                    { name:"金色的草地"}
-                ]
-            },
-        ]
-    },
-    { name:"第三单元",
-        children: [
-            { name:"阅读",
-                children: [
-                    { name:"金色的草地"},
-                    { name:"金色的草地"},
-                    { name:"金色的草地"},
-                    { name:"金色的草地"}
-                ]
-            },
-        ]
-    },
-    { name:"第三单元",
-        children: [
-            { name:"阅读",
-                children: [
-                    { name:"金色的草地"},
-                    { name:"金色的草地"},
-                    { name:"金色的草地"},
-                    { name:"金色的草地"}
-                ]
-            },
-        ]
-    },
-];
-$(document).ready(function(){
-    $.fn.zTree.init($("#treeDemo"), setting, zNodes);
-    $.fn.zTree.init($("#treeDemo1"), setting, zNodes);
-    $.fn.zTree.init($("#treeDemo2"), setting, zNodes);
-    $.fn.zTree.init($("#treeDemo3"), setting, zNodes);
 
-    $('.list-trees .list-p').click(function(){
-        $(this).next('.content_wrap').slideToggle();
-        $(this).addClass('ztree_active').siblings('p').removeClass('ztree_active');
-        $(this).find('img').attr('src','img/red_minus.png').parent('p').siblings('p').find('img').attr('src','img/black_add.png');
-    });
-
-});
